ce199dd755e531da2db45298e1f688fd70533dcf
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-03-07 17:04:03 +0000

    store creation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React from 'react';
 // components
-import Navbar from "./components/Navbar";
-import CartContainer from "./components/CartContainer";
+import Navbar from './components/Navbar';
+import CartContainer from './components/CartContainer';
 // items
-import cartItems from "./cart-items";
+import cartItems from './cart-items';
 // redux stuff
+import { createStore } from 'redux';
+
+function reducer() {
+  console.log('shake and bake');
+}
+
+const store = createStore(reducer);
 
 function App() {
   // cart setup
--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -1,38 +1,39 @@
-import React from "react";
-import CartItem from "./CartItem";
+import React from 'react';
+import CartItem from './CartItem';
+
 const CartContainer = ({ cart = [] }) => {
   if (cart.length === 0) {
     return (
-      <section className="cart">
+      <section className='cart'>
         {/* cart header */}
         <header>
           <h2>your bag</h2>
-          <h4 className="empty-cart">is currently empty</h4>
+          <h4 className='empty-cart'>is currently empty</h4>
         </header>
       </section>
     );
   }
   return (
-    <section className="cart">
+    <section className='cart'>
       {/* cart header */}
       <header>
         <h2>your bag</h2>
       </header>
       {/* cart items */}
       <article>
-        {cart.map(item => {
+        {cart.map((item) => {
           return <CartItem key={item.id} {...item} />;
         })}
       </article>
       {/* cart footer */}
       <footer>
         <hr />
-        <div className="cart-total">
+        <div className='cart-total'>
           <h4>
             total <span>$0.00</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className='btn clear-btn'>clear cart</button>
       </footer>
     </section>
   );
